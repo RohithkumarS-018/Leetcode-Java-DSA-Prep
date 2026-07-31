95f18326089c93370bb2854c943c20890378005d
RohithkumarS-018 <rohiths.01806@gmail.com>
2026-07-31 11:32:14 +0530

    update version

--- Arrays/215_Kth_Largest_Element_in_an_Array.java
+++ Arrays/215_Kth_Largest_Element_in_an_Array.java
@@ -1,14 +1,22 @@
 // LeetCode 215 - Kth Largest Element in an Array
-// Difficulty: Medium level coding problem
-// https://leetcode.com/problems/kth-largest-element-in-an-array/
+// Difficulty: Medium
+
+// Approach:
+// 1. Store all elements in a LinkedList.
+// 2. Sort the list in descending order using Collections.reverseOrder().
+// 3. Return the element at index (k - 1).
+
+// Time Complexity: O(n log n)
+// Space Complexity: O(n)
+
 import java.util.*;
 
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         LinkedList<Integer> list = new LinkedList<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            list.add(nums[i]);
+        for (int num : nums) {
+            list.add(num);
         }
 
         Collections.sort(list, Collections.reverseOrder());
