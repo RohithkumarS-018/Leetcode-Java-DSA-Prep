83a725eb8ef8b924adb780ca4dd6f6d28a9ac3b5
RohithkumarS-018 <rohiths.01806@gmail.com>
2026-08-02 07:44:42 +0530

    update

--- Arrays/001_Two_Sum.java
+++ Arrays/001_Two_Sum.java
@@ -13,8 +13,7 @@
 import java.util.HashMap;
 
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-
+    public int[] twoSum(int[] nums, int target) {                                
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
