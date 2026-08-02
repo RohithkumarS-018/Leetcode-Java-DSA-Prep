5eb84ac06d651ddc67d548aa45097f21c3bdbafb
RohithkumarS-018 <rohiths.01806@gmail.com>
2026-08-02 08:28:45 +0530

    adding

--- Queue/347_Top_K_Frequent_Elements.java
+++ Queue/347_Top_K_Frequent_Elements.java
@@ -15,14 +15,15 @@ class Solution {
     public int[] topKFrequent(int[] arr, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int num : arr) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
+map.put(num, map.getOrDefault(num, 0) + 1);    
+    }
         PriorityQueue<Integer> q = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
         q.addAll(map.keySet());
         int[] ans = new int[k];
         for (int i = 0; i < k; i++) {
             ans[i] = q.poll();
         }
+
         return ans;
     }
 }
