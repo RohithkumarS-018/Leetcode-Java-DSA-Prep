13ead4d8957a22a8f33c153bebbd1366d109271e
RohithkumarS-018 <rohiths.01806@gmail.com>
2026-08-04 22:59:13 +0530

    update

--- LinkedList/206_Reverse_Linked_List.java
+++ LinkedList/206_Reverse_Linked_List.java
@@ -18,17 +18,22 @@ Space Complexity: O(1)
 
 /**
  * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) {
- *         this.val = val;
- *         this.next = next;
- *     }
- * }
  */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
 
 class Solution {
     public ListNode reverseList(ListNode head) {
